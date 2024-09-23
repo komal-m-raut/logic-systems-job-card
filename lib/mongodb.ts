95bcc662a833c3ba0f8f10316b42b2a25f77e3ea
Komal <komalraut3102@gmail.com>
2024-09-23 22:30:47 +0530

    feat: add server code

---
+++ lib/mongodb.ts
@@ -0,0 +1,28 @@
+// lib/dbConnect.ts
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI as string;
+
+if (!MONGODB_URI) {
+  throw new Error(
+    'Please define the MONGODB_URI environment variable inside .env.local'
+  );
+}
+
+// lib/mongodb.ts
+let isConnected: boolean = false; // Track the connection status
+
+export async function dbConnect() {
+  if (isConnected) {
+    return;
+  }
+
+  try {
+    await mongoose.connect(MONGODB_URI);
+    isConnected = true;
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    throw new Error('MongoDB connection failed');
+  }
+}
